da0c1da881a2f4ad1069f08a91a0a6e0bfe20a45
tori-calkins <tori.calkins2705@gmail.com>
2023-10-17 20:21:33 -0700

    front end till css

--- preparedness-frontend/src/App.js
+++ preparedness-frontend/src/App.js
@@ -1,22 +1,31 @@
-import React from "react"
+import React, {useState} from "react"
 import { Button, Input, Label } from "reactstrap"
 import ModalComponent from "./components/ModalComponent"
 import "./App.css"
 
 const App = () => {
+  const [personName, setPersonName] = useState("")
+  const namePassed = (nameOfPerson) => { // passes the name of the person as the parameter of the useState hook
+    setPersonName(nameOfPerson.target.value) // set the persons name from the input and targets each name as the input gets changed.
+  }
+
+  const reset = () => {
+    setPersonName("")
+  }
+
   return (
-    <div className="entire-content">
+    <body><div className="entire-content">
       <h1>Preparedness Assessment</h1>
       <div className="form">
         <div className="input">
-          <Label for="name">Enter your name</Label>
-          <Input />
+          <Label for="name">Enter your name: </Label>
+          <Input value={personName} onChange={namePassed}  />
         </div>
-        <Button>Click Me</Button>
-        <Button>Reset</Button>
-        <ModalComponent />
+        <Button onClick={reset}>Reset</Button>
+        <ModalComponent personName={personName} /> 
       </div>
     </div>
+    </body>
   )
 }
 
